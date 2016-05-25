73dc5be3163fe15cdbade5a68d031dd1f3dd796c
Bachir Çaoui <bachir@followanalytics.com>
2016-05-26 01:55:55 +0200

    Add basic configuration for express app

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 5000;
+var router = express.Router();
+
+router.use(function(req, res, next) {
+  // log each request to the console
+  console.log('Request : ' +  req.method, req.url);
+  next();
+});
+
+router.post('/pull_request', function(req, res) {
+  console.log('Pull request received');
+});
+
+router.get('/', function(req, res) {
+  res.send('Nothing to see here :p');
+});
+
+app.use('/', router);
+
+app.listen(port, function() {
+  console.log('Listening on ' + port);
+});
