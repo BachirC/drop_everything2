8fe0d1b2430f7ca233731017cc1d08d13eaa9411
Bachir Çaoui <bachir@followanalytics.com>
2016-05-27 02:01:24 +0200

    Add handlers for different pull request actions (open and edit)

--- index.js
+++ index.js
@@ -12,9 +12,20 @@ router.use(function(req, res, next) {
   next();
 });
 
+// Main route. Gets all the data from github webhook
 router.post('/pull_request', function(req, res) {
-  params = extract_params(req.body);
-  console.log('Message : ' + params['description'] + ' At url : ' + params['url']);
+  action = req.body.action;
+
+  switch(action) {
+    case 'opened':
+      opening_handler(req.body);
+      break;
+    case 'edited':
+      editing_handler(req.body);
+      break;
+    default:
+      console.log('Action not handled');
+  }
   res.send('Received!');
 });
 
@@ -24,7 +35,7 @@ router.get('/', function(req, res) {
 
 app.use('/', router);
 
-var extract_params = function(body) {
+var opening_params = function(body) {
   var params = {
     description: body.pull_request.body,
     url: body.pull_request.html_url
@@ -32,6 +43,15 @@ var extract_params = function(body) {
   return params;
 };
 
+// Handlers for the different actions on pull requests (opened, edited, closed, synchronize, reopened)
+var opening_handler = function(body) {
+  params = opening_params(body);
+  console.log('You have opened a new PR at url : ' + params['url'] + ' with the description : ' + params['description']);
+};
+
+var editing_handler = function(body) {
+  console.log('PR edited');
+};
 
 app.listen(port, function() {
   console.log('Listening on ' + port);
