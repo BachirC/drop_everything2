8df4aa56d6b9eb402b5c255dc5e66b9664466d36
Bachir Çaoui <bachir@followanalytics.com>
2016-06-02 00:22:39 +0200

    use es6 syntax for PRReviewComment class

--- models/webhook_handlers/pull_request_review_comment.js
+++ models/webhook_handlers/pull_request_review_comment.js
@@ -1,38 +1,41 @@
+'use strict';
 const CREATED_ACTION = 'created';
 const ALLOWED_ACTIONS = [CREATED_ACTION];
 const PR_REVIEW_COM_TYPE = 'pull_request_review_comment';
 
 var Parser = require('../../models/comment_parser.js');
 
-var PRReviewComment = function(data, action) {
-  this.data = data;
-  this.action = action;
-};
+class PRReviewComment {
 
-PRReviewComment.prototype.sendInfo = function () {
-  return extractInfo(this.data);
-};
+  constructor (data, action) {
+    this.data = data;
+    this.action = action;
+  }
 
-PRReviewComment.prototype.validateAction = function () {
-  return (ALLOWED_ACTIONS.indexOf(this.action) !== -1);
-};
+  sendInfo () {
+    return this.extractInfo(this.data);
+  }
 
-var extractInfo = function (data) {
-  diff_hunk = data['comment']['diff_hunk']
-  return {
-    'action' : this.action,
-      'type' : PR_REVIEW_COM_TYPE,
-      'repo' : data['repository']['name'],
-      'pr_title' : data['pull_request']['title'],
-      'pr_url' : data['pull_request']['html_url'],
-      'pr_owner' : data['pull_request']['user']['login'],
+  validateAction () {
+    return (ALLOWED_ACTIONS.indexOf(this.action) !== -1);
+  }
+
+  extractInfo (data) {
+    return {
+      'action'     : this.action,
+      'type'       : PR_REVIEW_COM_TYPE,
+      'repo'       : data['repository']['name'],
+      'pr_title'   : data['pull_request']['title'],
+      'pr_url'     : data['pull_request']['html_url'],
+      'pr_owner'   : data['pull_request']['user']['login'],
       'recipients' : Parser.gitToSlack([data['pull_request']['user']['login'].substr(1)]),
       'com_author' : data['comment']['user']['login'],
-      'com_url' : data['comment']['html_url'],
-      'com_body' : data['comment']['body'],
-      'diff_hunk' : diff_hunk
-  };
-};
+      'com_url'    : data['comment']['html_url'],
+      'com_body'   : data['comment']['body'],
+      'diff_hunk'  : data['comment']['diff_hunk']
+    };
+  }
+}
 
 
 module.exports = PRReviewComment;
